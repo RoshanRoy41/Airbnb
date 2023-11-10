432cbfe2f6a8055e42d974ac605c31a783b6d0ef
Aaron Paul <aaron.paul@experionglobal.local>
2023-11-10 23:12:25 +0530

    feat: added review section

--- UnitCardFirstHLF/Unit.js
+++ UnitCardFirstHLF/Unit.js
@@ -7,7 +7,7 @@ getImages
   .then((data) => {
     data.forEach((element) => {
       images.push(element);
-      //   console.log(images);
+      console.log(images);
       //   console.log(images.download_url);
     });
     //random images for unit
@@ -45,6 +45,62 @@ getCountry
   })
   .catch((error) => console.log("Error:", error));
 
+const url = "https://dummyjson.com/users";
+
+const getData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    const showData = document.querySelector(".reviewsTexts");
+    let startingNumber = Math.floor(Math.random() * 25);
+    document.getElementById("imgHost").src = data.users[startingNumber].image;
+
+    for (
+      let i = startingNumber;
+      i < startingNumber + 4 && i < data.users.length;
+      i++
+    ) {
+      const userData = data.users[i];
+      const oneReview = createReviewCard(userData);
+      showData.appendChild(oneReview);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+getData(url);
+
+const createReviewCard = (userData) => {
+  const oneReview = document.createElement("div");
+  oneReview.classList.add("oneReview");
+
+  const oneReviewName = document.createElement("h3");
+  oneReviewName.classList.add("oneReview-Name");
+  const oneReviewRating = document.createElement("h3");
+  oneReviewName.classList.add("oneReview-Rating");
+
+  const oneReviewPara = document.createElement("p");
+  oneReviewPara.setAttribute("id", "oneReviewPara");
+
+  oneReviewName.textContent =
+    userData.firstName +
+    "  " +
+    userData.lastName +
+    " | " +
+    (1 + Math.random() * 4).toFixed(2);
+  oneReviewPara.textContent =
+    "Lorem ipsum  quis nostrud exercitation . Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  // oneReviewRating.textContent = " | " + (1 + Math.random() * 4).toFixed(2);
+
+  oneReview.appendChild(oneReviewName);
+  // oneReview.appendChild(oneReviewRating);
+  oneReview.appendChild(oneReviewPara);
+
+  return oneReview;
+};
+
 // async function fetchData() {
 //   try {
 //     const response = await fetch("https://picsum.photos/200/300");
@@ -55,3 +111,44 @@ getCountry
 //   }
 // }
 // fetchData();
+// const getNamesforReview = fetch("https://dummyjson.com/users/");
+// // console.log(getNamesforReview);
+
+// getNamesforReview
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     const usersForReview = [];
+//     data.users.forEach((element) => {
+//       usersForReview.push(element);
+//       console.log(usersForReview.firstName);
+//     });
+//     let reviewsTexts = document.querySelector(".reviewsTexts");
+
+//     for (let i = 0; i < 4; i++) {
+//       const oneReview = document.createElement("div");
+//       oneReview.classList.add("oneReview");
+//       const oneReviewName = document.createElement("h4");
+//       oneReviewName.classList.add("oneReview-Name");
+//       const oneReviewPara = document.createElement("p");
+//       oneReviewPara.setAttribute("id", "oneReviewPara");
+//     }
+//     document.querySelector(
+//       ".oneReview-Name"
+//     ).textContent = `${usersForReview[0].firstName} ${usersForReview[0].lastName}`;
+//     document.getElementById(
+//       "oneReviewPara"
+//     ).textContent = `Lorem ipsum  quis nostrud exercitation . Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+//     oneReview.appendChild(oneReviewName);
+//     oneReview.appendChild(oneReviewPara);
+//     reviewsTexts.appendChild(oneReview);
+//   })
+//   .catch((error) => console.error("Error:", error));
+//getting julias ID
+// const urlParam = new URLSearchParams(window.location.search);
+
+// const id = Number(urlParam.get("id"));
+// console.log(typeof id);
+
+// dataProcess(url, id);
