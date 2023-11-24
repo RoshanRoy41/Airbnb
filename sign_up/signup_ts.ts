e9b0954ffc65b0e8e907469f44482b4949370d0e
Hariprasad J <hariprasad.j@experionglobal.local>
2023-11-24 14:04:03 +0530

    feat:added signup page,contact page,changed into typescript

---
+++ sign_up/signup_ts.ts
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const signUpForm = document.querySelector('.login-form') as HTMLFormElement;
+
+    signUpForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        // Remove existing error messages
+        removeErrorMessages();
+
+        // Get form input values
+        const name = (document.getElementById('name') as HTMLInputElement).value;
+        const email = (document.getElementById('email') as HTMLInputElement).value;
+        const dob = (document.getElementById('dob') as HTMLInputElement).value;
+        const profession = (document.getElementById('profession') as HTMLInputElement).value;
+        const password = (document.getElementById('password') as HTMLInputElement).value;
+        const confirmPassword = (document.getElementById('confirmPassword') as HTMLInputElement).value;
+
+        // Validate all fields are entered
+        if (!name || !email || !dob || !profession || !password || !confirmPassword) {
+            displayErrorMessage('All fields are required.', 'name');
+            return;
+        }
+
+        // Validate password length
+        if (password.length < 7) {
+            displayErrorMessage('Password must be at least 7 characters.', 'password');
+            return;
+        }
+
+        // Validate password and confirm password match
+        if (password !== confirmPassword) {
+            displayErrorMessage('Password and Confirm Password do not match.', 'confirmPassword');
+            return;
+        }
+
+        // If all validations pass, you can proceed with further actions (e.g., form submission)
+        // alert('Sign-up successful!'); // Uncomment this line if you want to show an alert
+        // Here you can add code to submit the form or perform other actions
+        submitForm();
+    });
+
+    function displayErrorMessage(message: string, fieldId: string) {
+        const errorElement = document.createElement('div');
+        errorElement.classList.add('error-message');
+        errorElement.innerText = message;
+
+        const field = document.getElementById(fieldId) as HTMLElement;
+        field.parentElement?.appendChild(errorElement);
+    }
+
+    function removeErrorMessages() {
+        const errorMessages = document.querySelectorAll('.error-message');
+        errorMessages.forEach(message => message.remove());
+    }
+
+    function submitForm() {
+        const scriptURL = "https://script.google.com/macros/s/AKfycbyMR2j8nCu9_lUVc2KMs66_MmiH7DgCrHifoWDjXRgFCEB6Jw39SZxS8E_pT6E2pwii1Q/exec";
+        const formData = new FormData(signUpForm);
+
+        fetch(scriptURL, { method: "POST", body: formData })
+            .then((response) => response.json())
+            .then((data) => {
+                alert("Sign up is successfully completed.");
+                window.location.reload(); // Reload the page immediately
+            })
+            .catch((error) => {
+                console.error("Error!", error.message);
+                // Handle error appropriately (e.g., display an error message to the user)
+            });
+    }
+});
